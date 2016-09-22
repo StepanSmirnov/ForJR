401e0cd163d45734032afb6e98feedf3ebb1c7b4
Smirnov Stepan <sssprogrmaer@gmail.com>
2016-09-22 20:54:23 +0300

    Добавлена выдача исключений.

--- YandexTranslator.rb
+++ YandexTranslator.rb
@@ -1,6 +1,5 @@
 class YandexTranslator
 	require 'net/http'
-	require 'uri'
 	require "openssl"
 	def initialize(apikey)
 		@key=apikey		
@@ -9,10 +8,10 @@ class YandexTranslator
  		@http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 	end
 	private def scan(text,pattern)
-		if text.index('code="200"')||(text.index('Item')) 
+		if text.index('code="200"')||(text.index('Lang')) 
 			text[pattern] 
 		else 
-			text[/code=.*(?=\/>)/]  
+			raise ArgumentError,text[/code=.*(?=\/>)/]  
 		end		
 	end
 	def translate(text,lang,format=nil,options=nil)
@@ -25,14 +24,14 @@ class YandexTranslator
 		request.set_form_data({key:@key,text:text,hint:hint}) 
 		scan(@http.request(request).body,/(?<=lang=").*(?=")/)
 	end
-	def getlangs(ui)
+	def getlangs(uilang)
 		request = Net::HTTP::Post.new('/api/v1.5/tr/getLangs')
 		request.set_form_data({key:@key,ui:ui}) 
 		tmp=scan(@http.request(request).body,/(?<=<Item).*(?=\/><\/langs)/)
 		if tmp
 			tmp.split('/><Item').map{|x| Hash[x[/(?<=key=")\S*(?=")/].to_sym,x[/(?<=value=").*(?=")/]]} 
 		else 
-			"Parameter 'ui' is invalid" 
+			raise ArgumentError,"Parameter 'ui' is invalid" 
 		end
 	end
 end
