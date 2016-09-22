773e87f388fb5e0fa2529e4da20bb722a38dda99
Smirnov Stepan <sssprogrmaer@gmail.com>
2016-09-22 19:08:34 +0300

    Добавлен парсинг ответа.

--- YandexTranslator.rb
+++ YandexTranslator.rb
@@ -11,20 +11,20 @@ class YandexTranslator
 	def translate(text,lang,format=nil,options=nil)#,format,options
 		request = Net::HTTP::Post.new('/api/v1.5/tr/translate')
 		request.set_form_data({key:@key,text:text,lang:lang,format:format,options:options}) 
-		@http.request(request).body
+		@http.request(request).body[/(?<=text>).*(?=<\/text)/]
 	end
 	def detect(text,hint=nil)#,hint
 		request = Net::HTTP::Post.new('/api/v1.5/tr/detect')
 		request.set_form_data({key:@key,text:text,hint:hint}) 
-		@http.request(request).body
+		@http.request(request).body[/(?<=lang=").*(?=")/]
 	end
 	def getlangs(ui)
 		request = Net::HTTP::Post.new('/api/v1.5/tr/getLangs')
 		request.set_form_data({key:@key,ui:ui}) 
-		@http.request(request).body		
+		@http.request(request).body[/(?<=<Item).*(?=\/><\/langs)/].split('/><Item')	.map{|x| Hash[x[/(?<=key=")\S*(?=")/].to_sym,x[/(?<=value=").*(?=")/]]}	
 	end
 end
 tr=YandexTranslator.new('trnsl.1.1.20160907T145726Z.184b07fb1c05d3de.bf989596de0117c3f66b2af3204496f9bdf642ee')
-puts tr.translate('hell','ru')
-#puts tr.getlangs('mn')
-#puts tr.detect 'bonjour'
+#puts tr.translate('зажигалка','ru')
+puts tr.getlangs('mn')
+#puts tr.detect 'fast'
