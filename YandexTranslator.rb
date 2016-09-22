4454e16693ee8efd9fbd5b09b8f984c661d7f7d9
Smirnov Stepan <sssprogrmaer@gmail.com>
2016-09-22 20:19:03 +0300

    Добавлены сообщения об ошибках.

--- YandexTranslator.rb
+++ YandexTranslator.rb
@@ -8,23 +8,31 @@ class YandexTranslator
 		@http.use_ssl = true
  		@http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 	end
-	def translate(text,lang,format=nil,options=nil)#,format,options
+	private def scan(text,pattern)
+		if text.index('code="200"')||(text.index('Item')) 
+			text[pattern] 
+		else 
+			text[/code=.*(?=\/>)/]  
+		end		
+	end
+	def translate(text,lang,format=nil,options=nil)
 		request = Net::HTTP::Post.new('/api/v1.5/tr/translate')
 		request.set_form_data({key:@key,text:text,lang:lang,format:format,options:options}) 
-		@http.request(request).body[/(?<=text>).*(?=<\/text)/]
-	end
-	def detect(text,hint=nil)#,hint
+		scan(@http.request(request).body,/(?<=text>).*(?=<\/text)/)
+		end
+	def detect(text,hint=nil)
 		request = Net::HTTP::Post.new('/api/v1.5/tr/detect')
 		request.set_form_data({key:@key,text:text,hint:hint}) 
-		@http.request(request).body[/(?<=lang=").*(?=")/]
+		scan(@http.request(request).body,/(?<=lang=").*(?=")/)
 	end
 	def getlangs(ui)
 		request = Net::HTTP::Post.new('/api/v1.5/tr/getLangs')
 		request.set_form_data({key:@key,ui:ui}) 
-		@http.request(request).body[/(?<=<Item).*(?=\/><\/langs)/].split('/><Item')	.map{|x| Hash[x[/(?<=key=")\S*(?=")/].to_sym,x[/(?<=value=").*(?=")/]]}	
+		tmp=scan(@http.request(request).body,/(?<=<Item).*(?=\/><\/langs)/)
+		if tmp
+			tmp.split('/><Item').map{|x| Hash[x[/(?<=key=")\S*(?=")/].to_sym,x[/(?<=value=").*(?=")/]]} 
+		else 
+			"Parameter 'ui' is invalid" 
+		end
 	end
 end
-tr=YandexTranslator.new('trnsl.1.1.20160907T145726Z.184b07fb1c05d3de.bf989596de0117c3f66b2af3204496f9bdf642ee')
-#puts tr.translate('зажигалка','ru')
-puts tr.getlangs('mn')
-#puts tr.detect 'fast'
