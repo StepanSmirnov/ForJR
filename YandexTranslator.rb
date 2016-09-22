ff90b1c00986ed5cfa878ef1a22186e0c5daedd5
Smirnov Stepan <sssprogrmaer@gmail.com>
2016-09-22 23:05:57 +0300

    Метод getlangs выдает результат в виде хеша код_языка:полное_название

--- YandexTranslator.rb
+++ YandexTranslator.rb
@@ -14,22 +14,23 @@ class YandexTranslator
 			raise ArgumentError,text[/code=.*(?=\/>)/]  
 		end		
 	end
-	def translate(text,lang,format=nil,options=nil)
+	def translate(text,lang,format = nil,options = nil)
 		request = Net::HTTP::Post.new('/api/v1.5/tr/translate')
 		request.set_form_data({key:@key,text:text,lang:lang,format:format,options:options}) 
 		scan(@http.request(request).body,/(?<=text>).*(?=<\/text)/)
 		end
-	def detect(text,hint=nil)
+	def detect(text,hint = nil)
 		request = Net::HTTP::Post.new('/api/v1.5/tr/detect')
 		request.set_form_data({key:@key,text:text,hint:hint}) 
 		scan(@http.request(request).body,/(?<=lang=").*(?=")/)
 	end
 	def getlangs(uilang)
 		request = Net::HTTP::Post.new('/api/v1.5/tr/getLangs')
-		request.set_form_data({key:@key,ui:ui}) 
-		tmp=scan(@http.request(request).body,/(?<=<Item).*(?=\/><\/langs)/)
-		if tmp
-			tmp.split('/><Item').map{|x| Hash[x[/(?<=key=")\S*(?=")/].to_sym,x[/(?<=value=").*(?=")/]]} 
+		request.set_form_data({key:@key,ui:uilang}) 
+		if (tmp = scan(@http.request(request).body,/(?<=<Item).*(?=\/><\/langs)/))
+			hash=Hash.new()
+			tmp.split('/><Item').each{ |x| hash[x[/(?<=key=")\S*(?=")/].to_sym]=x[/(?<=value=").*(?=")/] } 
+			hash
 		else 
 			raise ArgumentError,"Parameter 'ui' is invalid" 
 		end
