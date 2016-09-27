7d20e6599b84cb06447f04b08aad8c836fcd15a3
Smirnov Stepan <sssprogrmaer@gmail.com>
2016-09-27 09:17:02 +0300

    Получаемый ответ в теперь формате json, добавлен собственный класс иcключений.

--- YandexTranslator.rb
+++ YandexTranslator.rb
@@ -1,38 +1,52 @@
+class YandexError < RuntimeError
+  def initialize(mes,code)
+    super(mes)
+    @code = code
+  end
+
+  def code
+    @code
+  end
+end
+
 class YandexTranslator
-	require 'net/http'
-	require "openssl"
-	def initialize(apikey)
-		@key=apikey		
-		@http = Net::HTTP.new('translate.yandex.net', 443)
-		@http.use_ssl = true
- 		@http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-	end
-	private def scan(text,pattern)
-		if text.index('code="200"')||(text.index('Lang')) 
-			text[pattern] 
-		else 
-			raise ArgumentError,text[/code=.*(?=\/>)/]  
-		end		
-	end
-	def translate(text,lang,format = nil,options = nil)
-		request = Net::HTTP::Post.new('/api/v1.5/tr/translate')
-		request.set_form_data({key:@key,text:text,lang:lang,format:format,options:options}) 
-		scan(@http.request(request).body,/(?<=text>).*(?=<\/text)/)
-		end
-	def detect(text,hint = nil)
-		request = Net::HTTP::Post.new('/api/v1.5/tr/detect')
-		request.set_form_data({key:@key,text:text,hint:hint}) 
-		scan(@http.request(request).body,/(?<=lang=").*(?=")/)
-	end
-	def getlangs(uilang)
-		request = Net::HTTP::Post.new('/api/v1.5/tr/getLangs')
-		request.set_form_data({key:@key,ui:uilang}) 
-		if (tmp = scan(@http.request(request).body,/(?<=<Item).*(?=\/><\/langs)/))
-			hash=Hash.new()
-			tmp.split('/><Item').each{ |x| hash[x[/(?<=key=")\S*(?=")/].to_sym]=x[/(?<=value=").*(?=")/] } 
-			hash
-		else 
-			raise ArgumentError,"Parameter 'ui' is invalid" 
-		end
-	end
+    require 'net/http'
+    require 'openssl'
+    require 'json'
+
+    class Connection
+      def initialize(key)
+        @key = key
+        @http = Net::HTTP.new('translate.yandex.net', 443)
+        @http.use_ssl = true
+        @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      end
+
+      def request(path, parameters)
+        request = Net::HTTP::Post.new("/api/v1.5/tr.json/#{path}")
+        request.set_form_data({key:@key}.merge(parameters)) 
+        tmp = JSON.parse(@http.request(request).body)
+        if (tmp['code'] == nil) || (tmp['code'] == 200)
+          tmp
+        else
+          raise YandexError.new(tmp['message'], tmp['code'])
+        end
+      end
+    end
+
+    def initialize(apikey)
+        @con = Connection.new(apikey)     
+    end
+    
+    def translate(text, lang, format = nil, options = nil)
+      @con.request('translate', {text:text, lang:lang, format:format, options:options})    
+    end
+
+    def detect(text, hint = nil)
+      @con.request('detect', {text:text, hint:hint}) 
+    end
+
+    def getlangs(uilang)
+      @con.request('getLangs', {ui:uilang})        
+    end
 end
